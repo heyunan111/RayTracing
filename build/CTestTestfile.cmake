977345eaf9d0198c596070de4c87a12cfa321492
hyn <he_yu_nan@qq.com>
2023-11-02 11:27:08 +0800

    create

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/27427/Desktop/vscodeProject/RayTracing
+# Build directory: C:/Users/27427/Desktop/vscodeProject/RayTracing/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
